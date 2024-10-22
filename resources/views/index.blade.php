4ba8e93b82589b22c06225143f08c5cc53608d04
qwertzys <142578957+qwertzys@users.noreply.github.com>
2024-10-22 17:31:24 +0700

    Lagi upload file di branch "testBranch" (Pt.1)
    app, bootstrap, config, database, public, resources

---
+++ resources/views/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <!-- Link to CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/indexStyle.css">
+</head>
+<body>
+<!-- NavBar -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <!-- Home BUtton -->
+    <a class="navbar-brand" href="#">Blue World</a> 
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!-- <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li> -->
+        <!-- Topics -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Topics</a>
+          <ul class="dropdown-menu">
+            <!-- Sea Pollution, Plastic Waste, Ocean Plastic, and Facts & Statistics -->
+            <li><a class="dropdown-item" href="#">Sea Pollution</a></li>
+            <li><a class="dropdown-item" href="#">Plastic Waste</a></li>
+            <li><a class="dropdown-item" href="#">Ocean Plastic</a></li>
+            <li><a class="dropdown-item" href="#">Facts & Statistics</a></li>
+          </ul>
+        </li>
+        <!-- Updates -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Updates</a>
+          <ul class="dropdown-menu">
+            <!-- Latest News and Current Campaigns -->
+            <li><a class="dropdown-item" href="#">Latest News</a></li>
+            <li><a class="dropdown-item" href="#">Current Campaigns</a></li>
+          </ul>
+        </li>
+        <!-- How You Can Help -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">How You Can Help</a>
+          <ul class="dropdown-menu">
+            <!-- Reduce Plastic Waste, Discussion Forum, and Help Clean the Sea -->
+            <li><a class="dropdown-item" href="#">Reduce Plastic</a></li>
+            <li><a class="dropdown-item" href="#">Discussion Forum</a></li>
+            <li><a class="dropdown-item" href="#">Help Clean the Sea</a></li>
+          </ul>
+        </li>
+        <!-- About Us -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About Us</a>
+          <ul class="dropdown-menu">
+            <!-- Company Profile, FAQ, Contact Us -->
+            <li><a class="dropdown-item" href="#">Company Profile</a></li>
+            <li><a class="dropdown-item" href="#">FAQ</a></li>
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+          </ul>
+        </li>
+      </ul>
+      <!-- Search Bar -->
+       <!-- I gotta make this work? Really??? -->
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- The Content of the Main Page -->
+ <div>
+    <section>
+
+    </section>
+
+    <section>
+        
+    </section>
+ </div>
+
+    <!-- Link to JS Bootstrap-->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
